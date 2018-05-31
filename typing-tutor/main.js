13c18bcce2c2dc2cf0dde875b1a7cf05f2ef0c26
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-05-31 13:05:04 -0700

    Created an Array of Objects representing the characters in the phrase. #1

---
+++ typing-tutor/main.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+var sentence = 'grumpy wizards make toxic brew for the evil queen and jack.'
+
+var characters = sentence.split('').map(function (characters) {
+  return {
+    characters
+  }
+})
